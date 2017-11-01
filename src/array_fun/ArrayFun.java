c661c11190eb16fadc3c0308199194d2d1a2f6d6
compsci <compsci@compsci>
2017-11-01 09:49:00 -0600

    initial commit, just the framework

---
+++ src/array_fun/ArrayFun.java
@@ -0,0 +1,10 @@
+package array_fun;
+
+public class ArrayFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/array_fun/TheMainClass.java
@@ -0,0 +1,10 @@
+package array_fun;
+
+public class TheMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
